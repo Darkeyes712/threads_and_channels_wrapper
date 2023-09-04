a57ab7f59a56ad544676ad6ff5b6bd27b7ce3185
Nikola Kolev <nikolakolev712@gmail.com>
2023-09-04 14:27:48 +0300

    adding docs adding notes adding a second producer/receiver instance but with i32

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,23 @@ use std::thread;
 use std::time::Duration;
 use threads::{Consume, Produce, ProducerConsumer};
 
+/// Represents a producer that generates random `f64` values.
 struct NumberProducer;
+
+/// Represents a consumer that processes `f64` values.
 struct NumberConsumer;
 
+/// Represents a producer that generates sequential `i32` values.
+struct NumberProducer2;
+
+/// Represents a consumer that processes `i32` values.
+struct NumberConsumer2;
+
 impl Produce<f64> for NumberProducer {
+    /// Produces 10 random `f64` values, sends them to a channel, and simulates work between sends.
+    ///
+    /// # Arguments
+    /// * `tx`: A Sender through which produced `f64` values are sent.
     fn produce(&self, tx: Sender<f64>) {
         let mut rng = rand::thread_rng();
         let vec_dat: Vec<f64> = (0..=10).map(|_| rng.gen::<f64>()).collect();
@@ -25,6 +38,10 @@ impl Produce<f64> for NumberProducer {
 }
 
 impl Consume<f64> for NumberConsumer {
+    /// Consumes `f64` values from a channel and prints them.
+    ///
+    /// # Arguments
+    /// * `rx`: A Receiver from which `f64` values are received.
     fn consume(&self, rx: Receiver<f64>) {
         for received in rx.iter() {
             println!("Consumed: {:?}", received);
@@ -32,8 +49,40 @@ impl Consume<f64> for NumberConsumer {
     }
 }
 
+impl Produce<i32> for NumberProducer2 {
+    /// Produces sequential `i32` values from 0 to 9, sends them to a channel, and simulates work between sends.
+    ///
+    /// # Arguments
+    /// * `tx`: A Sender through which produced `i32` values are sent.
+    fn produce(&self, tx: Sender<i32>) {
+        for i in 0..=9 {
+            println!("Produce_2: {}", i);
+            tx.send(i).unwrap();
+            // Simulating some work
+            thread::sleep(Duration::from_millis(500));
+        }
+    }
+}
+
+impl Consume<i32> for NumberConsumer2 {
+    /// Consumes `i32` values from a channel and prints them.
+    ///
+    /// # Arguments
+    /// * `rx`: A Receiver from which `i32` values are received.
+    fn consume(&self, rx: Receiver<i32>) {
+        for received in rx.iter() {
+            println!("Consumed_2: {:?}", received);
+        }
+    }
+}
+
 fn main() {
     let pc = ProducerConsumer::new(NumberProducer, NumberConsumer);
+    let _pc_2 = ProducerConsumer::new(NumberProducer2, NumberConsumer2);
     pc.join_threads();
+    _pc_2.join_threads();
     // Here, you can do other things in the main thread if needed.
 }
+
+// Explore different apporaches to having the data from the threads available from the outside of the thread itself.
+// Add another trait/struct combination to the threads.rs file that contains a different approach to the data aquisition.
--- src/threads.rs
+++ src/threads.rs
@@ -1,21 +1,34 @@
-use std::sync::mpsc::{self, Receiver, Sender}; // <-- Make sure Receiver is imported here.
+use std::sync::mpsc::{self, Receiver, Sender};
 use std::thread;
 
+/// A generic struct to manage the communication between a producer and consumer using channels.
 pub struct ProducerConsumer<T: Send + 'static> {
-    tx: Sender<T>, // This will be usefull later!
+    /// Sender end of the channel. Useful for sending more messages in the future.
+    tx: Sender<T>,
+    /// Handle to the producer thread, allowing for its management after creation.
     producer_handle: Option<thread::JoinHandle<()>>,
+    /// Handle to the consumer thread, allowing for its management after creation.
     consumer_handle: Option<thread::JoinHandle<()>>,
 }
 
+/// A trait defining the behavior of a producer.
+///
+/// Types that implement this trait should be able to produce items of type `T`
+/// and send them to a given `Sender`.
 pub trait Produce<T: Send> {
     fn produce(&self, tx: Sender<T>);
 }
 
+/// A trait defining the behavior of a consumer.
+///
+/// Types that implement this trait should be able to consume items of type `T`
+/// from a given `Receiver`.
 pub trait Consume<T: Send> {
     fn consume(&self, rx: Receiver<T>);
 }
 
 impl<T: Send + 'static> ProducerConsumer<T> {
+    /// Waits for both the producer and consumer threads to finish their execution.
     pub fn join_threads(self) {
         if let Some(handle) = self.producer_handle {
             handle.join().unwrap();
@@ -24,6 +37,13 @@ impl<T: Send + 'static> ProducerConsumer<T> {
             handle.join().unwrap();
         }
     }
+
+    /// Creates a new ProducerConsumer setup. Starts threads for both producer and consumer.
+    ///
+    /// # Arguments
+    ///
+    /// * `producer` - An object implementing the `Produce` trait.
+    /// * `consumer` - An object implementing the `Consume` trait.
     pub fn new(
         producer: impl Produce<T> + Send + 'static,
         consumer: impl Consume<T> + Send + 'static,
